c7a663bddb2cec84488a459bcce6b830a9f1eea4
TaiBK15 <tainguyentp2505@gmail.com>
2019-07-19 18:56:22 +0700

    Algorithm to separate string

--- MyAndroidTest/app/src/main/java/com/example/myandroidtest/MainActivity.java
+++ MyAndroidTest/app/src/main/java/com/example/myandroidtest/MainActivity.java
@@ -8,7 +8,6 @@ import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private MyRecyclerViewAdapter mAdapter;
     private ArrayList<String> mKeyword;
     private final String TAG = "MainActivity";
+    private char c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +37,12 @@ public class MainActivity extends AppCompatActivity {
      * Generate keywords for items from local json file
      */
     private void createKeyword(){
+        String correctKeyword = null;
         try {
             JSONArray m_jArr = new JSONArray(loadJSONFromAsset());
             for (int i = 0; i <= m_jArr.length(); i++){
-                mKeyword.add(m_jArr.getString(i));
+                correctKeyword = processKeyword(m_jArr.getString(i));
+                mKeyword.add(correctKeyword);
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -65,5 +67,53 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
         return json;
+    }
+
+    /**
+     * Separate string into 2 sub strings (if need)
+     * Require: Difference between length of 2 sub strings is minimum
+     * Add character new line into string "\n" to separate
+     */
+    private String processKeyword(String inputKeyword){
+        int len = inputKeyword.length(),
+            lenSub1 = 0,
+            lenSub2 = 0,
+            posLine = 0,
+            minDiff = len;
+        //Count words from String
+        String[] words = inputKeyword.split(" ");
+        if(words.length == 1)
+            return inputKeyword;
+        else if(words.length == 2)
+            return inputKeyword.replace(" ", "\n");
+        else{
+            for (String word : words){
+                Log.d(TAG + "word", word);
+                lenSub1 = lenSub1 + word.length();
+                lenSub2 = len - lenSub1 - 1; //Remove space between 2 sub strings
+                if(Math.abs(lenSub1 - lenSub2) <= minDiff){
+                    minDiff = Math.abs(lenSub1 - lenSub2);
+                    //Add amount of space between words
+                    //Get position to replace character new line '\n'
+                    posLine = lenSub1++;
+                }
+                else
+                    return replaceCharAtIndex(inputKeyword, posLine, '\n');
+            }
+            return inputKeyword;
+        }
+
+    }
+
+    /**
+     * Replace character at specific index in String
+     * @param oldString
+     * @param pos
+     * @param c
+     * @return
+     */
+    private String replaceCharAtIndex(String oldString, int pos, char c){
+        String newString = oldString.substring(0, pos) + c + oldString.substring(pos+1);
+        return  newString;
     }
 }
